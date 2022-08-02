9e33390b4de380975c9f9c3d0c2a416d7e74cacb
Tempestfury11 <Swiftyblue11@gmail.com>
2022-08-02 09:21:15 +0200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-app.listen(3000)
+const port = parseInt(process.env.PORT) || 4000;
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+})
 
 // Data
 let courses = [
